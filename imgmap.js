9156adcb39c0f71bcde6e6584cfb77e436385c29
Chris Pomerville <CFourPO@users.noreply.github.com>
2017-08-29 19:02:19 -0400

    fixing for mobile

---
+++ imgmap.js
@@ -0,0 +1,30 @@
+let imgmap = {
+    spike_shadow_01: {
+        dev: "https://rawgit.com/CFourPO/7489c268dc22a2db1978c775d2abc81b/raw/484b2e8e4a056461c2b5a0690d6f0efbde727b50/spike_shadow_0.1.svg",
+        prod: "https://cdn.rawgit.com/CFourPO/7489c268dc22a2db1978c775d2abc81b/raw/484b2e8e4a056461c2b5a0690d6f0efbde727b50/spike_shadow_0.1.svg"
+    },
+    spike_01: {
+        dev: "https://rawgit.com/CFourPO/b389fdd5687276e154afd5e8a2bf0e6f/raw/d4ff6f82c65eeb5811ff5dd3280ec6b69da0fae7/spike_0.1.svg",
+        prod: "https://cdn.rawgit.com/CFourPO/b389fdd5687276e154afd5e8a2bf0e6f/raw/d4ff6f82c65eeb5811ff5dd3280ec6b69da0fae7/spike_0.1.svg"
+    },
+    bird_sm_shadow_01: {
+        dev: "https://rawgit.com/CFourPO/c40df0c86de125d8648cc4479f690a70/raw/5f4a0427bb5c4d3b37fbf27dcd5016fb796ba276/bird_sm_shadow_0.1.svg",
+        prod: "https://cdn.rawgit.com/CFourPO/c40df0c86de125d8648cc4479f690a70/raw/5f4a0427bb5c4d3b37fbf27dcd5016fb796ba276/bird_sm_shadow_0.1.svg"
+    },
+    bird_sm_01: {
+        dev: "https://rawgit.com/CFourPO/f0db475776e07b5950ccf9c63ad0d3f4/raw/2d2ffc72f0b5931f6ae6596ce22754047b57b19f/bird_sm_0.1.svg",
+        prod: "https://cdn.rawgit.com/CFourPO/f0db475776e07b5950ccf9c63ad0d3f4/raw/2d2ffc72f0b5931f6ae6596ce22754047b57b19f/bird_sm_0.1.svg"
+    },
+    bird_lg_shadow_01: {
+        dev: "https://rawgit.com/CFourPO/49b6574cfec2f083b2a884c1d05a6bb8/raw/da838125676e2def1832ba2a425dd300a3bdff23/bird_lg_shadow_0.1.svg",
+        prod: "https://cdn.rawgit.com/CFourPO/49b6574cfec2f083b2a884c1d05a6bb8/raw/da838125676e2def1832ba2a425dd300a3bdff23/bird_lg_shadow_0.1.svg"
+    },
+    bird_lg_01: {
+        dev: "https://rawgit.com/CFourPO/ac7cb355d45d93fa64f2b56c91b88ffa/raw/bf26723805d3e09c33cbb2f37b1133c82fc8080e/bird_lg_0.1.svg",
+        prod: "https://cdn.rawgit.com/CFourPO/ac7cb355d45d93fa64f2b56c91b88ffa/raw/bf26723805d3e09c33cbb2f37b1133c82fc8080e/bird_lg_0.1.svg"
+    },
+    bird: {
+        dev: "https://rawgit.com/CFourPO/9a429d9797830622ca5362161e58bde9/raw/c49b290f686b63bb71b31f6a14bac767ca064cad/bird.svg",
+        prod: "https://cdn.rawgit.com/CFourPO/9a429d9797830622ca5362161e58bde9/raw/c49b290f686b63bb71b31f6a14bac767ca064cad/bird.svg"
+    }
+}
